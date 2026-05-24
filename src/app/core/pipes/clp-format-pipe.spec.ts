488ed6058aae0d825848bcfba0fb17a06105baed
Laceration <be.moretti@duocuc.cl>
2026-05-23 20:35:32 -0400

    Changed cart colors and added a pipe to format price

---
+++ src/app/core/pipes/clp-format-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ClpFormatPipe } from './clp-format-pipe';
+
+describe('ClpFormatPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ClpFormatPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
